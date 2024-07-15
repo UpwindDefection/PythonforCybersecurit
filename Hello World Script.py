c2e2c097ed83f677dcb80782fafbf8f419b60003
UpwindDefection <joyce.anthony@student.greenriver.edu>
2024-07-15 06:20:06 +0000

    Create Hello World Script.py

---
+++ Hello World Script.py
@@ -0,0 +1,17 @@
+#Takes a user input for a variable named "name"
+name = input("What is your name?: ")
+
+#Printing the name inputted
+print(f"Hello, {name}")
+
+#Prints a positive message
+print("Today will be a good day!")
+
+#Asks the user for their age
+age = int(input("How old are you?: ")) #changes the input to an integer rather than a string
+
+#Creats a variable named "agein2years" by taking the input of "age" and adding 2 to it 
+agein2years = (age + 2)
+
+#prints the age in 2 years
+print(f"In two years you will be, {agein2years}")
